498c0ae5ec588576c61bac2766c77e5f9ee4c183
jsn <julien.saintmartin@wi6labs.com>
2015-03-28 17:33:44 +0100

    final maisonMix version, Youpi

--- ARDUINO_SKETCH/cogarden/cogarden.ino
+++ ARDUINO_SKETCH/cogarden/cogarden.ino
@@ -3,26 +3,26 @@
 #include <math.h>
 #include "DHT.h"
 #define DHTPIN A2
-#include <SerialLCD.h>
+//#include <SerialLCD.h>
 #include <SoftwareSerial.h> //this is a must
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-SerialLCD slcd(6,7);  
+//SerialLCD slcd(6,7);  
 //AirQuality airqualitysensor;
 //int current_quality =-1;
 int pin=5;
 
 void setup() {
-  float Rsensor_light;
   int sensorValue_hydro = 0;
   
    LoRa_init();
-  LoRa_RF_config(4); slcd.begin();
+  LoRa_RF_config(4); 
+  
+  //slcd.begin();
   
   dht.begin();
-  pinMode(A0, OUTPUT);  
-  slcd.print("hello, world!");
+  //slcd.print("hello, world!");
  // airqualitysensor.init(14);
  
  Serial.begin(9600);
@@ -30,17 +30,27 @@ void setup() {
 
 void loop() {
  String msg;
- int light_sensorvalue = analogRead(A0);                         // value of light_sensor
-int Rsensor=(float)(1023-light_sensorvalue)*10/light_sensorvalue;          // resistance ?
-Serial.print("Rsensor");
-Serial.println(Rsensor);
- 
+ led();
+ int light_sensorvalue = analogRead(A0); // value of light_sensor
+// delay(30000);
+ Serial.print("light");
+ Serial.println(light_sensorvalue);
+ //double voltage = (5 / 1024)* light_sensorvalue;
+ //double r = (20 * voltage)/(5 - voltage);
+ //Serial.print("voltage");
+  //Serial.println(voltage);
+ //Serial.print("r");
+//Serial.println(r);
+ //int lux = (int )(500 / r);
+// float Vout = light_sensorvalue*0.0048828125;
+//int lux = ((500*(5-Vout))/(10*Vout));
+ //int lux = light_sensorvalue * 100;
   int sensorValue_hydro = analogRead(A1);            // Value of hydrometrie
   
  float h = dht.readHumidity();
  float t = dht.readTemperature();             // value temperature et humidity
 
-
+aff_panel(light_sensorvalue, h, sensorValue_hydro, t);
   //current_quality=airqualitysensor.slope();   // value of airquality; value return = 1 hight pollu, 3 fresh air;;
    
  Serial.print("ligth");
@@ -58,25 +68,25 @@ Serial.println(Rsensor);
  //Serial.print("air quality");
  //Serial.println(current_quality);
   Serial.println("-----------------------------------");
-  msg  = "LoraFabian:{";
+  msg  = "LoraFabian:";
 
-  msg += "\"light\":\"";
+  msg += "light=";
   msg +=   light_sensorvalue;
-  msg += "\",";
+  msg += "&";
 
-  msg += "\"hydro\":\"";
+  msg += "hydro=";
   msg +=   sensorValue_hydro;
-  msg += "\",";
+  msg += "&";
 
-  msg += "\"humidity\":\"";
+  msg += "humidity=";
   msg +=   h;
-  msg += "\",";
+  msg += "&";
 
-  msg += "\"temperature\":\"";
+  msg += "temperature=";
   msg +=   t;
   
   
-  msg += "\"}";
+ // msg += "";
 
 
   
