f4f7e3f543ada16212a58ffed2938e1a5e9c6799
Renzo Navas <renzoe@gmail.com>
2015-06-09 18:16:03 +0200

    * reply to beacon coap. ready to change the MAC and beacon-coap response of the node

--- examples/lorafabian/lorafab_beacon_answer_new/lorafab_beacon_answer_new.c
+++ examples/lorafabian/lorafab_beacon_answer_new/lorafab_beacon_answer_new.c
@@ -1,33 +1,6 @@
 /*--------------------------------------------------------------------------
 
-              j]_                   .___                                   
-._________    ]0Mm                .=]MM]=                                  
-M]MM]MM]M]1  jMM]P               d]-' NM]i                                 
--~-~   4MM1  d]M]1              d]'   jM]'                                 
-       j]MT .]M]01       d],  .M]'    d]#                                  
-       d]M1 jM4M]1  .,  d]MM  d]I    .]M'                                  
-       ]0]  M/j]0(  d]L NM]f d]P     jM-                                   
-       M]M .]I]0M  _]MMi -' .]M'                                           
-       M]0 jM MM]  jM-M>   .]M/                                            
-       ]0F MT ]M]  M>      d]M1        .,                                  
-      j0MT.]' M]M j]1 .mm .]MM ._d]_,   J,                                 
-      jM]1jM  ]01 =] .]M/ jM]Fd]M]MM]   .'                                 
-      j]M1#T .M]1.]1 jM]' M]0M/^ "M]MT  j         .",    .__,  _,-_        
-      jMM\]' J]01jM  M]M .]0]P    ]0]1  i         1 1   .'  j .'  "1       
-      j]MJ]  jM]1]P .]M1 jMMP     MM]1  I        J  t   1   j J    '       
-      =M]dT  jM]q0' dM]  M]MT     ]MM  j        j   j  j    J 1            
-      ]M]M`  j]0j#  ]MF  ]M]'    .M]P  J       .'   j  J  .J  4_,          
-      M]0M   =MM]1 .M]'  MM]     jM](  1       r    j  1  --,   "!         
-      ]0MT   ]M]M  jM@   ]M]     M]P  j       J     j j     4     1        
-      MM]'   M]0P  j]1  .M]M    j]M'  J      j'     ",?     j     1        
-     _]M]    M]0`  jM1 .MNMM,  .]M'   1     .'       11     1    j'        
-     jM]1   jM]@   j]L_]'?M]M__MP'    \     J        1G    J    .'         
-     j]0(   jM]1   "M]P'  "N]M/-      "L__J L________'?L__- *__,'          
-     "-'    "--                                                            
-                                                                           
-----------------------------------------------------------------------------
-
-Copyright (c) <2014>, <Wi6labs>
+Copyright (c) <2015>, <Wi6labs> ,  <Telcom Bretagne>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -37,14 +10,14 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    * Neither the name of the wi6labs nor the
+    * Neither the name of Telecom Bretagne, wi6labs nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL WI6LABS BE LIABLE FOR ANY
+DISCLAIMED. IN NO EVENT SHALL TELECOM BRETAGNE OR WI6LABS BE LIABLE FOR ANY
 DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -53,8 +26,10 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-Description: Simple example to test LoRa RX. It set sx1272 in receive mode on the
+Description:
+LoRaFabian Beacon Answer code. LoRa RX setq sx1272 in receive mode on the
 channel configured in sx1272_contiki_radio.c
+
 -----------------------------------------------------------------------------*/                                                             
 
 #include "contiki.h"
