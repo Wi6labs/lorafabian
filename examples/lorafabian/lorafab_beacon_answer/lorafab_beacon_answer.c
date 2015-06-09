c900783bb8d046156aa5f46268b482bd10adc501
Renzo Navas <renzoe@gmail.com>
2015-06-09 15:44:01 +0200

    lorafab beacon answer:
    * now we use erbium engino for CoAP
    * we added a -hardcoded- 802.15.5 mac

--- examples/lorafabian/lorafab_beacon_answer/lorafab_beacon_answer.c
+++ examples/lorafabian/lorafab_beacon_answer/lorafab_beacon_answer.c
@@ -1,33 +1,6 @@
 /*--------------------------------------------------------------------------
 
-              j]_                   .___                                   
-._________    ]0Mm                .=]MM]=                                  
-M]MM]MM]M]1  jMM]P               d]-' NM]i                                 
--~-~   4MM1  d]M]1              d]'   jM]'                                 
-       j]MT .]M]01       d],  .M]'    d]#                                  
-       d]M1 jM4M]1  .,  d]MM  d]I    .]M'                                  
-       ]0]  M/j]0(  d]L NM]f d]P     jM-                                   
-       M]M .]I]0M  _]MMi -' .]M'                                           
-       M]0 jM MM]  jM-M>   .]M/                                            
-       ]0F MT ]M]  M>      d]M1        .,                                  
-      j0MT.]' M]M j]1 .mm .]MM ._d]_,   J,                                 
-      jM]1jM  ]01 =] .]M/ jM]Fd]M]MM]   .'                                 
-      j]M1#T .M]1.]1 jM]' M]0M/^ "M]MT  j         .",    .__,  _,-_        
-      jMM\]' J]01jM  M]M .]0]P    ]0]1  i         1 1   .'  j .'  "1       
-      j]MJ]  jM]1]P .]M1 jMMP     MM]1  I        J  t   1   j J    '       
-      =M]dT  jM]q0' dM]  M]MT     ]MM  j        j   j  j    J 1            
-      ]M]M`  j]0j#  ]MF  ]M]'    .M]P  J       .'   j  J  .J  4_,          
-      M]0M   =MM]1 .M]'  MM]     jM](  1       r    j  1  --,   "!         
-      ]0MT   ]M]M  jM@   ]M]     M]P  j       J     j j     4     1        
-      MM]'   M]0P  j]1  .M]M    j]M'  J      j'     ",?     j     1        
-     _]M]    M]0`  jM1 .MNMM,  .]M'   1     .'       11     1    j'        
-     jM]1   jM]@   j]L_]'?M]M__MP'    \     J        1G    J    .'         
-     j]0(   jM]1   "M]P'  "N]M/-      "L__J L________'?L__- *__,'          
-     "-'    "--                                                            
-                                                                           
-----------------------------------------------------------------------------
-
-Copyright (c) <2014>, <Wi6labs>
+Copyright (c) <2015>, <Wi6labs> ,  <Telcom Bretagne>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -37,14 +10,14 @@ modification, are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright
       notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    * Neither the name of the wi6labs nor the
+    * Neither the name of Telecom Bretagne, wi6labs nor the
       names of its contributors may be used to endorse or promote products
       derived from this software without specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
 ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL WI6LABS BE LIABLE FOR ANY
+DISCLAIMED. IN NO EVENT SHALL TELECOM BRETAGNE OR WI6LABS BE LIABLE FOR ANY
 DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
 (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
 LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -53,8 +26,10 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-Description: Simple example to test LoRa RX. It set sx1272 in receive mode on the
+Description:
+LoRaFabian Beacon Answer code. LoRa RX setq sx1272 in receive mode on the
 channel configured in sx1272_contiki_radio.c
+
 -----------------------------------------------------------------------------*/                                                             
 
 #include "contiki.h"
@@ -67,14 +42,28 @@ channel configured in sx1272_contiki_radio.c
 
 #include "arduino_spi.h"
 
+#include "er-coap.h"
+#include "er-coap-constants.h"
+
 static struct etimer rx_timer;
 
 static u8 rx_msg[256];	
+static uint8_t tx_buffer[512];
+int tx_buffer_index;
+
+/* Example URIs that can be queried. */
+
+#define NUMBER_OF_URLS 4
+/* leading and ending slashes only for demo purposes, get cropped automatically when setting the Uri-Path */
+char *service_urls[NUMBER_OF_URLS] =
+{ "/","1.n.t", "auth", ".well-known/core" };
+
 
 void coap_beacon_send_response(){
 
 	//5045525D-C0FF6E6F-64653D36-36
-	uint8_t databuff_coap[] = {	0x50,  // 0101 0000 . Version 01 , Type Non-Confirmable: 1. Token Lenght: 0
+	uint8_t databuff_coap[] = {	0x41,0xc8,0xd1,0xfa,0xb1,0x00,0x01,0xaa,0x66,0x6b,0x00,0x00,0x00,0x00,0x01, // 802 15 4 HEADER
+								0x50,  // 0101 0000 . Version 01 , Type Non-Confirmable: 1. Token Lenght: 0
 								0x45,  // Code: 2.05 . Content (69)
 								0x52,0x5d, // TODO: Message ID (must match received)
 								0xc0, // Opt #1: Content-Format; text/plain. Opt 12. 1100 (C=12) 0000 (0 Lenght)
@@ -84,15 +73,38 @@ void coap_beacon_send_response(){
 	};
 
 	// TODO: encapsulate in 802.15.4 Frame? or its contiki work?
+	tx_buffer_index = 0;
 
-	lora_radio_driver.send(databuff_coap, sizeof(databuff_coap));
+	uint8_t header_802_15_4[] = {0x22,0x27,0x01,0xfa,0x81,0xca,0xca,0x00,0x00,0x00,0x00,0x00,0x01}; // TODO Frame real 80215
+
+	memcpy(tx_buffer, header_802_15_4 , sizeof(header_802_15_4));
+	tx_buffer_index += sizeof(header_802_15_4);
+
+	// COAP
+	//coap_packet_t coap_packet;
+	size_t coap_packet_size;
+	static coap_packet_t coap_request[1];      /* This way the packet can be treated as pointer as usual. */
+
+	coap_init_message(coap_request, COAP_TYPE_NON, COAP_POST, coap_get_mid()); // random_rand();
+	//coap_set_header_uri_path(coap_request, service_urls[0]);
+	//coap_set_header_uri_host(coap_request, service_urls[1]);
+
+
+
+	char coap_payload[] = "{\"n\":\"alpha.s.ackl.io\"}";
+	coap_set_payload(coap_request, (uint8_t *)coap_payload, sizeof(coap_payload) - 1);
+
+	coap_packet_size = coap_serialize_message(coap_request, (void *)(tx_buffer + tx_buffer_index));
+	tx_buffer_index += coap_packet_size;
+
+	lora_radio_driver.send(tx_buffer, tx_buffer_index);
 
 }
 
 
 void respond_if_coap_beacon( u8 rx_msg[] , int size){
 
-	int mac_header_size = 15;
+	int mac_header_size = 13;
 	int iaux;
 
 	if (size >= mac_header_size ) {
@@ -117,12 +129,13 @@ void respond_if_coap_beacon( u8 rx_msg[] , int size){
 	printf("\n\r\tHeader SEQ (dont care): %02x", rx_msg[2]);
 
 	// 2 ) Analyse 802.15.4 PANID
-	printf("\n\r\tPanID (must be 1101?): %02x%02x", rx_msg[3], rx_msg[4]);
+	//printf("\n\r\tPanID (must be FAB1): %02x%02x", rx_msg[3], rx_msg[4]);
 	
 	// 3 ) Addresses
-	printf("\n\r\tADDR Dest: %02x%02x", rx_msg[5], rx_msg[6]);
+	printf("\n\r\tADDR Dest: ");
+	for (iaux = 0 ; iaux < 8; iaux++ ) printf("%02x", rx_msg[3+iaux]);
 	printf("\n\r\tADDR Src: ");
-	for (iaux = 0 ; iaux < 8; iaux++ ) printf("%02x", rx_msg[7+iaux]);
+	for (iaux = 0 ; iaux < 2; iaux++ ) printf("%02x", rx_msg[11+iaux]);
 
 	// 4 ) Check CoAP message
 	printf("\n\r\tPayload: ");
@@ -132,13 +145,15 @@ void respond_if_coap_beacon( u8 rx_msg[] , int size){
 
 	/* CoAP Packet  to send */
 	/* Constrained Application Protocol, Non-Confirmable, GET, MID:21085 ( 0x525d )*/
-
+	/*
 	uint8_t databuff_coap[] = {0x50,0x01,0x52,0x5d,0xb4,0x6e,0x5f,0x69,0x64};
 	unsigned int  databuff_coap_size = 9;
-
-	if ( ( size - mac_header_size ) == (databuff_coap_size) ){
+	int size_comp = ( size - mac_header_size ) == (databuff_coap_size);
+	size_comp = 1;
+	if ( size_comp ){
 
 		int comp = memcmp(  & (rx_msg[mac_header_size]) , databuff_coap , databuff_coap_size);
+		comp = 0 ;
 		if ( comp == 0 ){
 			printf("This is the LoRA CoAP Beacon");
 
@@ -153,6 +168,32 @@ void respond_if_coap_beacon( u8 rx_msg[] , int size){
 
 	} else{
 		printf("Not LoRa Beacon. Not same length");
+	}*/
+
+
+	 /* static declaration reduces stack peaks and program code size */
+	static coap_packet_t coap_message[1]; /* this way the packet can be treated as pointer as usual */
+	erbium_status_code = NO_ERROR;
+
+	erbium_status_code =
+	coap_parse_message(coap_message, (void *)(rx_msg + mac_header_size), size - mac_header_size);
+	if(erbium_status_code == NO_ERROR) {
+		int check_coap_beacon = (coap_message->type == COAP_TYPE_NON) && ((coap_message->code == COAP_POST));
+		if ( check_coap_beacon ){
+			printf("This is the LoRA CoAP Beacon");
+
+			// Here we should send this to another non blocking thread (or maybe blocking is preferable ? for not use the radio at same time)
+			// and implement a Collision avoidance algoritm (some random time), or rely on ContikiMAC?
+
+			coap_beacon_send_response();
+
+		}else{
+			printf("Not LoRa Beacon. Other CoAP Message.");
+		}
+
+
+	} else{
+		printf("Not LoRa Beacon. CoAP parse ERROR");
 	}
 
 
@@ -163,7 +204,7 @@ void respond_if_coap_beacon( u8 rx_msg[] , int size){
 
 }
 
-	/*---------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------*/
 PROCESS(lorafab_bcn_process, "LoRaFabian Beacon process");
 AUTOSTART_PROCESSES(&lorafab_bcn_process);
 /*---------------------------------------------------------------------------*/
@@ -175,8 +216,8 @@ PROCESS_THREAD(lorafab_bcn_process, ev, data)
 	int i;
 	int size;
 
-  leds_init();
-  leds_toggle(LEDS_ALL); 
+	leds_init();
+	leds_toggle(LEDS_ALL);
 
 	arduino_spi_init();
 
@@ -187,6 +228,7 @@ PROCESS_THREAD(lorafab_bcn_process, ev, data)
 
 		
 	etimer_set(&rx_timer, 5 * CLOCK_SECOND);
+
 
   while( 1 )
   {
