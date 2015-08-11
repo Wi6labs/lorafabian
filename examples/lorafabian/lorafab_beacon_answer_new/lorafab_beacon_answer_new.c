3c275aa58fb4a3c92fe0eecbe385e9c165109448
AmarOk <blin.sebastien@enconn.fr>
2015-08-11 14:06:09 +0200

    Improve lorafab_answer_new

--- examples/lorafabian/lorafab_beacon_answer_new/lorafab_beacon_answer_new.c
+++ examples/lorafabian/lorafab_beacon_answer_new/lorafab_beacon_answer_new.c
@@ -1,6 +1,6 @@
 /*--------------------------------------------------------------------------
 
-Copyright (c) <2015>, <Wi6labs> ,  <Telcom Bretagne>
+Copyright (c) <2015>, <Wi6labs>, <Telecom Bretagne>
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -36,179 +36,195 @@ channel configured in sx1272_contiki_radio.c
 #include "leds-arch.h"
 #include "stm32f10x.h"
 #include <dev/leds.h>
-#include <stdio.h> /* For printf() */
-//#include "sx1272_radio.h"
-#include "sx1272_contiki_radio.h"
-
+#include <stdio.h>
+#include "layer802154_radio_lora.h"
 #include "arduino_spi.h"
-
+#include "debug_on_arduino.h"
 #include "er-coap.h"
 #include "er-coap-constants.h"
 
+#include "frame802154_lora.h"
+#include "cfs/cfs.h"
+#include <string.h>
+
 static struct etimer rx_timer;
+static struct etimer timer_payload_beacon;
 
-static u8 rx_msg[256];
-static uint8_t tx_buffer[512];
-int tx_buffer_index;
+//The response to the beacon
+char coap_payload_beacon[150];
 
-// Modifi here: the MAC and the reply to coap_payload_beacon (the response)
-uint8_t header_802_15_4_hardcoded[] = {0x22,0x27,0x01,0xfa,0x81, // Don touch this
-							0xfa,0xba,0x00,0x00,0x00,0x00,0x00,0x03}; // This is our 8Byte MAC. TODO Frame real 80215
+static int is_associated = 0;
+static int is_beacon_receive = 0;
 
-char coap_payload_beacon[] = "{\"n\":\"gamma.s.ackl.io\"}";
-
-void coap_beacon_send_response(){
-
-	// TODO: encapsulate in 802.15.4 Frame? or its contiki work?
-	tx_buffer_index = 0;
-
-	// 802154 FRAME HEADER
-	//uint8_t header_802_15_4[] = header_802_15_4_hardcoded;
-
-	memcpy(tx_buffer, header_802_15_4_hardcoded , sizeof(header_802_15_4_hardcoded));
-	tx_buffer_index += sizeof(header_802_15_4_hardcoded);
-
-	// COAP
-	//coap_packet_t coap_packet;
-	size_t coap_packet_size;
-	static coap_packet_t coap_request[1];      /* This way the packet can be treated as pointer as usual. */
-
-	coap_init_message(coap_request, COAP_TYPE_NON, COAP_POST, coap_get_mid()); // random_rand();
-
-
-
-	//char coap_payload[] = coap_payload_beacon;
-	coap_set_payload(coap_request, (uint8_t *)coap_payload_beacon, sizeof(coap_payload_beacon) - 1);
-
-	coap_packet_size = coap_serialize_message(coap_request, (void *)(tx_buffer + tx_buffer_index));
-	tx_buffer_index += coap_packet_size;
-	printf("We are sending the response to the coap beacon\n");
-	int iaux;
-	for (iaux = 0 ; iaux < tx_buffer_index; iaux++ ) printf("%02x", tx_buffer[iaux]);
-
-	lora_radio_driver.send(tx_buffer, tx_buffer_index);
-
+/**
+ * \brief: update the hostname with /HOSTNAME file
+ */
+void updateHOSTNAME()
+{
+  char dns[150];//The content of the file
+  int fd;
+  //Read in /HOSTNAME_LORA
+  fd = cfs_open("/HOSTNAME_LORA", CFS_READ);
+  if(fd >= 0) {
+    //Read 500 char
+    cfs_read(fd, dns, sizeof(dns));
+    cfs_close(fd);
+    //Get the real hostname
+    int size = 0;
+    //Because the space significate the end of the hostname
+    while(dns[size] != '\0')
+      ++size;
+    //final = the real url
+    char final[size];
+    int i;
+    for(i = 0; i != sizeof(final) +1; ++i)
+      final[i] = dns[i];
+    strcpy(coap_payload_beacon, "{\"n\":\"");
+    strcat(coap_payload_beacon, final);
+    strcat(coap_payload_beacon, "\"}");
+  }
+  else {
+    printf("ERREUR LORS DE LA LECTURE\n\r");
+    strcpy(coap_payload_beacon, "{\"n\":\"default.test\"}");
+    return;
+  }
+  printf("HOSTNAME : %s\n\r", coap_payload_beacon);
 }
 
+/**
+ * \brief: Send the coap_payload_beacon to layer802154
+ */
+void
+coap_beacon_send_response() {
+  updateHOSTNAME();
+  uint8_t tx_buffer[512];
 
-void respond_if_coap_beacon( u8 rx_msg[] , int size){
+  size_t coap_packet_size;
+  static coap_packet_t coap_request[1];      //This way the packet can be treated as pointer as usual.
 
-	int mac_header_size = 13;
-	int iaux;
+  unsigned short random_a = random_rand();
+  char MAC[] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};
+  getMac(MAC);
+  unsigned short random_b = random_a ^ MAC[sizeof(MAC)-1];
+  coap_init_message(coap_request, COAP_TYPE_NON, COAP_POST, (coap_get_mid()+random_b)%65535);
+  int sizeMSG = strlen(coap_payload_beacon);
+  coap_set_payload(coap_request, (uint8_t *)coap_payload_beacon, sizeMSG);
 
-	if (size >= mac_header_size ) {
-		printf("Size OK");
-	} else{
-		printf("Size is NOT OK");
-		return;
-	}
+  coap_packet_size = coap_serialize_message(coap_request, (void *)(tx_buffer ));
+  int tx_buffer_index = coap_packet_size;
+  printf("We are sending the response to the coap beacon\n\r");
 
-	// 1 ) Analyse 802.15.4 HEADER
-	printf("\n\r\tHeader FCF (2227)?: %02x%02x", rx_msg[0], rx_msg[1]);
-	printf("\n\r\tHeader SEQ (dont care): %02x", rx_msg[2]);
+  //We must write a destination address on 8 bytes
+  uint8_t destAddr[] = {0xfa,0x01};
+  layer802154_send(tx_buffer, tx_buffer_index, destAddr, SIGNALISATION_ON, DST_SHORT_FLAG);
 
-	// 2 ) Analyse 802.15.4 PANID
-	//printf("\n\r\tPanID (must be FAB1): %02x%02x", rx_msg[3], rx_msg[4]);
-	
-	// 3 ) Addresses
-	printf("\n\r\tADDR Dest: ");
-	for (iaux = 0 ; iaux < 8; iaux++ ) printf("%02x", rx_msg[3+iaux]);
-	printf("\n\r\tADDR Src: ");
-	for (iaux = 0 ; iaux < 2; iaux++ ) printf("%02x", rx_msg[11+iaux]);
-
-	// 4 ) Check CoAP message
-	printf("\n\r\tPayload: ");
-	for (iaux= mac_header_size ; iaux< size; iaux++ ) printf("%02x", rx_msg[iaux]);
-
-	printf("\n\r\tPailod is CoAP Beacon?: ");
-
-	 /* static declaration reduces stack peaks and program code size */
-	static coap_packet_t coap_message[1]; /* this way the packet can be treated as pointer as usual */
-	erbium_status_code = NO_ERROR;
-
-	erbium_status_code =
-	coap_parse_message(coap_message, (void *)(rx_msg + mac_header_size), size - mac_header_size);
-	if(erbium_status_code == NO_ERROR) {
-		int check_coap_beacon = (coap_message->type == COAP_TYPE_NON) && ((coap_message->code == COAP_POST)
-								&& (coap_message->payload_len == 0));
-		if ( check_coap_beacon ){
-			printf("This is the LoRA CoAP Beacon");
-
-			// Here we should send this to another non blocking thread (or maybe blocking is preferable ? for not use the radio at same time)
-			// and implement a Collision avoidance algoritm (some random time), or rely on ContikiMAC?
-
-			coap_beacon_send_response();
-
-		}else{
-			printf("Not LoRa Beacon. Other CoAP Message.");
-		}
-
-
-	} else{
-		printf("Not LoRa Beacon. CoAP parse ERROR");
-	}
-
-
-
-
-
-	printf("\n\r");
-
+  is_associated = 1;
 }
 
-	/*---------------------------------------------------------------------------*/
+/**
+ * \brief: Check if the payload is a beacon payload
+ * \param: rx_msg, the payload
+ * \param: size, the payload length
+ */
+int respond_if_coap_beacon(u8 rx_msg[], int size) {
+  printf("\n\r\tPayload: ");
+  int iaux;
+  for(iaux = 0 ; iaux<size ; iaux++)
+    printf("%02x", rx_msg[iaux]);
+
+   //static declaration reduces stack peaks and program code size
+  static coap_packet_t coap_message[1]; //this way the packet can be treated as pointer as usual
+  erbium_status_code = NO_ERROR;
+
+  erbium_status_code =
+  coap_parse_message(coap_message, (void *)(rx_msg), size);
+  printf("\n\r\tPayload is CoAP Beacon?: ");
+  if(erbium_status_code == NO_ERROR) {
+    int check_coap_beacon = (coap_message->type == COAP_TYPE_NON) && ((coap_message->code == COAP_POST)
+                            && (coap_message->payload_len == 0));
+    if(check_coap_beacon){
+      printf("\n\r\tThis is the LoRA CoAP Beacon\n\r");
+      return 1;
+    } else
+        printf("Not LoRa Beacon. Other CoAP Message.\n\r");
+  } else
+      printf("Not LoRa Beacon. CoAP parse ERROR\n\r");
+  printf("\n\r");
+  return 0;
+}
+
+/*---------------------------------------------------------------------------*/
 PROCESS(lorafab_bcn_process, "LoRaFabian Beacon process");
 AUTOSTART_PROCESSES(&lorafab_bcn_process);
 /*---------------------------------------------------------------------------*/
-PROCESS_THREAD(lorafab_bcn_process, ev, data)
-{
+PROCESS_THREAD(lorafab_bcn_process, ev, data) {
   PROCESS_BEGIN();
 
-	int pending = 0;
-	int i;
-	int size;
+  int pending = 0;
+  int i;
+  int size;
 
   leds_init();
-  leds_toggle(LEDS_ALL); 
+  leds_toggle(LEDS_ALL);
+  arduino_spi_init();
+  layer802154_init();
+  //Start infinite RX
+  layer802154_on();
 
-	arduino_spi_init();
+  etimer_set(&rx_timer, 5*CLOCK_SECOND);
 
-	lora_radio_driver.init();
-
-	// Start infinite RX	
-	lora_radio_driver.on();
-
-		
-	etimer_set(&rx_timer, 5 * CLOCK_SECOND);
-
-  while( 1 )
-  {
-
+  while(1) {
     PROCESS_WAIT_EVENT();
 
-    if(ev == PROCESS_EVENT_TIMER) {
-			leds_toggle(LEDS_ALL);
+    if(is_beacon_receive && !is_associated && etimer_expired(&timer_payload_beacon))
+    {
+      etimer_stop(&timer_payload_beacon);
+      coap_beacon_send_response();
+    }
 
-			pending = lora_radio_driver.pending_packet();
-			printf("pending_packet: %d\n\r", pending );
+    if(etimer_expired(&rx_timer)) {
+      leds_toggle(LEDS_ALL);
 
-			if (pending) {
-				size = lora_radio_driver.read(rx_msg, sizeof(rx_msg));
-				printf("rx_msg, size: %d\n\r", size);
-				for (i = 0; i<size; i++) 
-					printf("%02x", rx_msg[i] );
+      pending = layer802154_pending_packet();
+      printf("pending_packet: %d\n\r", pending);
 
-				printf("\n\r");
+      if(pending) {
+        frame802154_lora_t frame = layer802154_read();
+        size = frame.payload_len;
 
+        if(frame.header_len == -1)
+          printf("Error: buffer is too small for headers");
+        else {
+          //For the arduino
+          int packetSize = size + frame.header_len;
+          //Verify the destination of a message
+          bool br_msg = is_broadcast_addr(&frame);
+          bool my_mac = is_my_mac(&frame);
+          if(br_msg) {
+            printf("Broadcast message");
+	    if(!is_signaling(&frame) || debug_on_arduino)
+              set_arduino_read_buf(frame.packet, packetSize);
+	  }
+          else if(my_mac) {
+            printf("Message is for me");
+            set_arduino_read_buf(frame.packet, packetSize);
+	  }
+          else {
+            printf("Message is not for me");
+	    if(debug_on_arduino)
+              set_arduino_read_buf(frame.packet, packetSize);
+          }
 
-				respond_if_coap_beacon(rx_msg , size);
-				//coap_beacon_send_response(); // for testing
-			}
-
-
-			//			printf( "test1\n\r");
-
-			etimer_reset(&rx_timer);
+	  //To avoid collision
+          if(respond_if_coap_beacon(frame.payload, size) && !is_associated)
+          {
+            is_beacon_receive = 1;
+            int random_timer = (random_rand()%30);
+            etimer_set(&timer_payload_beacon, random_timer*CLOCK_SECOND);
+          }
+        }
+      }
+      etimer_reset(&rx_timer);
     }
   }
   PROCESS_END();
